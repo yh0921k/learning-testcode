4a5bc4521fddff80a12154cfdaaaae50fb0c3919
Yonghwi Kim <yh0921k@gmail.com>
2021-03-24 11:11:42 +0900

    [add] 1장 첫 번째 JUnit 테스트 만들기 까지 완료
    - 제공 저장소의 iloveyouboss_04 패키지까지 진행 완료

--- src/test/java/p01/ScoreCollectionTest.java
+++ src/test/java/p01/ScoreCollectionTest.java
@@ -1,5 +1,7 @@
 package p01;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -8,5 +10,19 @@ public class ScoreCollectionTest {
   @Test
   public void test() {
     //fail("Not yet implemented");
+  }
+
+  @Test
+  public void answersArithmeticMeanOfTwoNumbers() {
+    // Arrange
+    ScoreCollection collection = new ScoreCollection();
+    collection.add(() -> 5);
+    collection.add(() -> 7);
+
+    // Act
+    int actualResult = collection.arithmeticMean();
+
+    // Assert
+    assertThat(actualResult, equalTo(6));
   }
 }
