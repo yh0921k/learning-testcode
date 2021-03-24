fedd0d33cfeb6808b1bce3374645d366d0969075
Yonghwi Kim <yh0921k@gmail.com>
2021-03-24 10:31:49 +0900

    [add] 1.2 JUnit의 기본 : 첫 번째 테스트 통과 까지 완료
    - 제공 저장소의 iloveyouboss_03 패키지까지 진행 완료

---
+++ src/test/java/p01/ScoreCollectionTest.java
@@ -0,0 +1,12 @@
+package p01;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ScoreCollectionTest {
+  @Test
+  public void test() {
+    //fail("Not yet implemented");
+  }
+}
