6e294effa6f9eb4ed3cf8132d86a090c32e675ce
Yonghwi Kim <yh0921k@gmail.com>
2021-03-25 14:58:57 +0900

    [add] 2.4 두 번째 테스트 만들기까지 완료
    - 제공 저장소의 iloveyouboss_10 패키지까지 진행 완료

--- src/test/java/p02/ProfileTest.java
+++ src/test/java/p02/ProfileTest.java
@@ -1,6 +1,7 @@
 package p02;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,17 +9,33 @@ public class ProfileTest {
 
   @Test
   public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-      Profile profile = new Profile("Bull Hockey, Inc.");
-      Question question = new BooleanQuestion(1, "Got bonuses?");
-      Answer profileAnswer = new Answer(question, Bool.FALSE);
-      profile.add(profileAnswer);
+    Profile profile = new Profile("Bull Hockey, Inc.");
+    Question question = new BooleanQuestion(1, "Got bonuses?");
+    Answer profileAnswer = new Answer(question, Bool.FALSE);
+    profile.add(profileAnswer);
 
-      Criteria criteria = new Criteria();
-      Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-      Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-      criteria.add(criterion);
+    Criteria criteria = new Criteria();
+    Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+    Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+    criteria.add(criterion);
 
-      boolean matches = profile.matches(criteria);
-      assertFalse(matches);
+    boolean matches = profile.matches(criteria);
+    assertFalse(matches);
+  }
+
+  @Test
+  public void matchAnswersTrueForAnyDontCareCriteria() {
+    Profile profile = new Profile("Bull Hockey, Inc.");
+    Question question = new BooleanQuestion(1, "Got milk?");
+    Answer profileAnswer = new Answer(question, Bool.FALSE);
+    profile.add(profileAnswer);
+
+    Criteria criteria = new Criteria();
+    Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+    Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+    criteria.add(criterion);
+
+    boolean matches = profile.matches(criteria);
+    assertTrue(matches);
   }
 }
